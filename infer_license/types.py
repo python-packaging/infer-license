a8a1d7e5d5ea5b12db8f5a3010ed67ef5e0db302
Tim Hatch <tim@timhatch.com>
2019-11-20 20:38:25 -0800

    Add Trove classifiers

--- infer_license/types.py
+++ infer_license/types.py
@@ -7,6 +7,7 @@ import pkg_resources
 class License:
     name: str
     shortname: str
+    trove_classifier: str
 
     @property
     def text(self) -> str:
@@ -14,20 +15,56 @@ class License:
             __name__, f"licenses/{self.shortname}.txt"
         ).decode()
 
-
+# See some discussion at https://github.com/pypa/warehouse/issues/2996 about
+# using SPDX names, which is what we use for shortname.  The two marked TODO
+# here don't have a current Trove classifier.
 KNOWN_LICENSES = [
-    License("Apache License 2.0", "Apache-2.0"),
-    License('BSD 2-Clause "Simplified" License', "BSD-2-Clause"),
-    License('BSD 3-Clause "New" or "Revised" License', "BSD-3-Clause"),
-    License("Creative Commons Attribution 4.0 International", "CC-BY-4.0"),
-    License("GNU Affero General Public License v3.0 or later", "AGPL-3.0-or-later"),
-    License("GNU Lesser General Public License v2 or later", "LGPL-2.0-or-later"),
-    License("GNU Lesser General Public License v2.1 or later", "LGPL-2.1-or-later"),
-    License("GNU Lesser General Public License v3.0 or later", "LGPL-3.0-or-later"),
-    License("GNU General Public License v2.0 or later", "GPL-2.0-or-later"),
-    License("GNU General Public License v3.0 or later", "GPL-3.0-or-later"),
-    License("MIT License", "MIT"),
-    License("MIT No Attribution", "MIT-0"),
+    License(
+        "Apache License 2.0",
+        "Apache-2.0",
+        "License :: OSI Approved :: Apache Software License",
+    ),
+    License(
+        'BSD 2-Clause "Simplified" License',
+        "BSD-2-Clause",
+        "License :: OSI Approved :: BSD License",
+    ),
+    License(
+        'BSD 3-Clause "New" or "Revised" License',
+        "BSD-3-Clause",
+        "License :: OSI Approved :: BSD License",
+    ),
+    License("Creative Commons Attribution 4.0 International", "CC-BY-4.0", "TODO"),
+    License(
+        "GNU Affero General Public License v3.0 or later",
+        "AGPL-3.0-or-later",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+    ),
+    License(
+        "GNU Lesser General Public License v2 or later",
+        "LGPL-2.0-or-later",
+        "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
+    ),
+    License(
+        "GNU Lesser General Public License v2.1 or later", "LGPL-2.1-or-later", "TODO"
+    ),
+    License(
+        "GNU Lesser General Public License v3.0 or later",
+        "LGPL-3.0-or-later",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+    ),
+    License(
+        "GNU General Public License v2.0 or later",
+        "GPL-2.0-or-later",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+    ),
+    License(
+        "GNU General Public License v3.0 or later",
+        "GPL-3.0-or-later",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+    ),
+    License("MIT License", "MIT", "License :: OSI Approved :: MIT License"),
+    License("MIT No Attribution", "MIT-0", "License :: OSI Approved :: MIT License"),
 ]
 
 
