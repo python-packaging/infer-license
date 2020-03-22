e0d98ef1c6115ac969e2c836ca6bc8f972a715bb
Tim Hatch <tim@timhatch.com>
2020-03-22 09:22:41 -0700

    Move to more static config

--- setup.py
+++ setup.py
@@ -2,8 +2,5 @@ from setuptools import setup
 
 setup(
     use_scm_version=True,
-    install_requires=["dataclasses >= 0.7; python_version < '3.7'",],
     entry_points={"console_scripts": ["infer_license = infer_license.cmdline:main"]},
-    package_data={"infer_license": ["py.typed"]},
-    include_package_data=True,
 )
