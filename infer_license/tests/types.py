e410e718880e07a60e92dbb6788bec40aefe135b
Ben Spoor <b.spoor@edna.eu>
2021-12-04 22:51:39 +0100

    Fix trove classifiers

--- infer_license/tests/types.py
+++ infer_license/tests/types.py
@@ -15,7 +15,27 @@ class TypesTest(unittest.TestCase):
             license.trove_classifier for license in types.KNOWN_LICENSES
         ]
 
+        # Below licenses are to broad, not a license or not part of SPDX-license list
+        not_covered = [
+            "License :: DFSG approved",
+            "License :: Free For Educational Use",
+            "License :: Free For Home Use",
+            "License :: Free for non-commercial use",
+            "License :: Free To Use But Restricted",
+            "License :: Freely Distributable",
+            "License :: Freeware",
+            "License :: GUST Font License 1.0",
+            "License :: GUST Font License 2006-09-30",
+            "License :: Nokia Open Source License (NOKOS)",
+            "License :: OSI Approved :: Eiffel Forum License",
+            "License :: OSI Approved :: Jabber Open Source License",
+            "License :: OSI Approved :: MITRE Collaborative Virtual Workspace License (CVW)",
+            "License :: Other/Proprietary License",
+            "License :: Public Domain",
+            "License :: Repoze Public License",
+        ]
+
         for classifier in classifiers:
-            if classifier.startswith('License ::'):
+            if classifier.startswith("License ::") and classifier not in not_covered:
                 with self.subTest(msg=classifier):
                     self.assertTrue(classifier in covered_licenses)
