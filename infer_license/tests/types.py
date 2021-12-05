78d4f1a3f844cb5660e71a28b86fc3d5da94f483
Ben Spoor <b.spoor@edna.eu>
2021-12-05 21:52:24 +0100

    Make sure all licenses are covered by infer-license

--- infer_license/tests/types.py
+++ infer_license/tests/types.py
@@ -9,3 +9,13 @@ class TypesTest(unittest.TestCase):
             if license.trove_classifier:
                 with self.subTest(msg=license.shortname):
                     self.assertTrue(license.trove_classifier in classifiers)
+
+    def test_all_licenses_classifiers_covered(self) -> None:
+        covered_licenses = [
+            license.trove_classifier for license in types.KNOWN_LICENSES
+        ]
+
+        for classifier in classifiers:
+            if classifier.startswith('License ::'):
+                with self.subTest(msg=classifier):
+                    self.assertTrue(classifier in covered_licenses)
